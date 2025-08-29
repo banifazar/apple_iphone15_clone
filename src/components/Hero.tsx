6e531fcc610ef5e8145c69b846a866e5eba95552
Bani Faza Rasyadan <bnfzrsydn2@gmail.com>
2025-08-29 22:04:55 +0700

    Add hero video

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,7 +1,27 @@
 import gsap from "gsap";
 import {useGSAP} from "@gsap/react";
+import {useEffect, useState} from "react";
+import {heroVideo, smallHeroVideo} from "../utils";
 
 const Hero = () => {
+    const [videoSrc, setVideoSrc] = useState(window.innerWidth < 768 ? smallHeroVideo : heroVideo);
+
+    const handleVideoSrcSet = () => {
+        if (window.innerWidth < 768) {
+            setVideoSrc(smallHeroVideo);
+        } else {
+            setVideoSrc(heroVideo);
+        }
+    }
+
+    useEffect(() => {
+        window.addEventListener("resize", handleVideoSrcSet);
+        return () => {
+            window.removeEventListener("resize", handleVideoSrcSet);
+        };
+    }, []);
+
+
     useGSAP(() => {
         gsap.to("#hero", {opacity: 1, duration: 4})
     }, [])
@@ -10,8 +30,14 @@ const Hero = () => {
         <section className="w-full nav-height bg-black relative">
             <div className="h-5/6 w-full flex-center flex-col">
                 <p id="hero" className="hero-title">iPhone 15 Pro</p>
+                <div className="md:w-10/12 w-9/12">
+                    <video className="pointer-events-none" autoPlay muted playsInline key={videoSrc}>
+                        <source src={videoSrc}/>
+                    </video>
+                </div>
             </div>
         </section>
     )
 }
 export default Hero
+
