24cb37ca25dbfec237a694ee7b68c130f1b4e5a7
Bani Faza Rasyadan <bnfzrsydn2@gmail.com>
2025-08-30 20:38:46 +0700

    Styling control button for video carousel

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,50 +1,64 @@
 import gsap from "gsap";
-import {useGSAP} from "@gsap/react";
-import {useEffect, useState} from "react";
-import {heroVideo, smallHeroVideo} from "../utils";
+import { useGSAP } from "@gsap/react";
+import { heroVideo, smallHeroVideo } from "../utils";
+import { useEffect, useState } from "react";
 
 const Hero = () => {
-    const [videoSrc, setVideoSrc] = useState(window.innerWidth < 768 ? smallHeroVideo : heroVideo);
+  const [videoSrc, setVideoSrc] = useState(
+    window.innerWidth < 760 ? smallHeroVideo : heroVideo,
+  );
 
-    const handleVideoSrcSet = () => {
-        if (window.innerWidth < 768) {
-            setVideoSrc(smallHeroVideo);
-        } else {
-            setVideoSrc(heroVideo);
-        }
+  const handleVideoSrcSet = () => {
+    if (window.innerWidth < 760) {
+      setVideoSrc(smallHeroVideo);
+    } else {
+      setVideoSrc(heroVideo);
     }
+  };
 
-    useEffect(() => {
-        window.addEventListener("resize", handleVideoSrcSet);
-        return () => {
-            window.removeEventListener("resize", handleVideoSrcSet);
-        };
-    }, []);
+  useEffect(() => {
+    window.addEventListener("resize", handleVideoSrcSet);
 
+    return () => {
+      window.removeEventListener("resize", handleVideoSrcSet);
+    };
+  }, []);
 
-    useGSAP(() => {
-        gsap.to("#hero", {opacity: 1, delay: 2})
-        gsap.to("#cta", {opacity: 1, y: -50, delay: 2})
-    }, [])
+  useGSAP(() => {
+    gsap.to("#hero", { opacity: 1, delay: 2 });
+    gsap.to("#cta", { opacity: 1, y: -50, delay: 2 });
+  }, []);
 
-    return (
-        <section className="w-full nav-height bg-black relative">
-            <div className="h-5/6 w-full flex-center flex-col">
-                <p id="hero" className="hero-title">iPhone 15 Pro</p>
+  return (
+    <section className="nav-height relative w-full bg-black">
+      <div className="flex-center h-5/6 w-full flex-col">
+        <p id="hero" className="hero-title">
+          iPhone 15 Pro
+        </p>
+        <div className="w-9/12 md:w-10/12">
+          <video
+            className="pointer-events-none"
+            autoPlay
+            muted
+            playsInline={true}
+            key={videoSrc}
+          >
+            <source src={videoSrc} type="video/mp4" />
+          </video>
+        </div>
+      </div>
 
-                <div className="md:w-10/12 w-9/12">
-                    <video className="pointer-events-none" autoPlay muted playsInline key={videoSrc}>
-                        <source src={videoSrc}/>
-                    </video>
-                </div>
+      <div
+        id="cta"
+        className="flex translate-y-20 flex-col items-center opacity-0"
+      >
+        <a href="#highlights" className="btn">
+          Buy
+        </a>
+        <p className="text-xl font-normal">From $199/month or $999</p>
+      </div>
+    </section>
+  );
+};
 
-                <div id="cta" className="flex flex-col items-center opacity-0 translate-y-20 mt-10 md:mt-5">
-                    <a href="#highlights" className="btn">Buy</a>
-                    <p className="font-normal text-xl">From $199/month or $999</p>
-                </div>
-            </div>
-        </section>
-    )
-}
-export default Hero
-
+export default Hero;
